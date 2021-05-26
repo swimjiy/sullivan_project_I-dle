998e0100448c717b3cb14370ea867444c71ad95a
alsxm <tulip23@naver.com>
2021-05-26 23:30:14 +0900

    [FEAT] API 호출 및 데이터 정제 #4
    API 호출 후 current 데이터를 정제하여 화면에 출력했습니다.
    또한 daily(예보) 데이터를 정제하여 리스트 형태로 저장했습니다.

--- script.js
+++ script.js
@@ -3,16 +3,6 @@ const datePicker = document.querySelector("#date-picker");
 const title = document.querySelector("#title");
 const weatherInfo = document.querySelector("#weather-info");
 
-// 지정된 날짜로 제목 변경
-const month = 5;
-const date = 22;
-title.innerHTML = `${month}월 ${date}일의 내 기상환경은?`;
-
-// 지정된 날짜에 대한 날짜 정보 출력
-const temperature = 18;
-const weather = "때로 흐림";
-const airPollution = "나쁨";
-weatherInfo.innerHTML = `${temperature}도 / ${weather} / 미세먼지 ${airPollution}`;
 
 // 드롭다운 옵션 동적 생성
 const dates = ["2021년 5월 21일", "2021년 5월 22일", "2021년 5월 23일"];
@@ -26,5 +16,73 @@ dates.map((date) => {
 
 // 날짜 선택 드롭다운 버튼 클릭해서 옵션 열고 닫기
 datePicker.addEventListener("click", (e) => {
-  dateCardWrapper.classList.toggle("hidden");
+  dateCardWrapper.classList.toggle("hcurrIdden");
 });
+
+function timestampToDate(timestamp) {
+  const fullDate = new Date(timestamp * 1000);
+
+  const month = fullDate.getMonth() + 1;
+  const date = fullDate.getDate();
+
+  return `${month}월 ${date}일`;
+}
+
+//khj
+function getWeather(lat, lon) {
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appId=2fa3f03d3732cc2adffbdcc9cd0ff4af`)
+    .then(function (response) {
+      return response.json()
+    }).then(function (data) {
+      //current
+      const curr = data.current;
+
+      const currTimestamp = curr.dt;
+      const currDate = timestampToDate(currTimestamp);
+      const currTemp = Math.round(curr.temp - 273.15);
+      const currWeatherId = curr.weather[0].id;
+      let currWeather = '';
+
+      title.innerHTML = `${currDate}의 내 기상환경은?`;
+
+
+      if (parseInt(currWeatherId / 100) === 2) {
+        currWeather = "천둥번개";
+      } else if (parseInt(currWeatherId / 100) === 3) {
+        currWeather = "이슬비"
+      } else if (parseInt(currWeatherId / 100) === 5) {
+        currWeather = "비"
+      } else if (parseInt(currWeatherId / 100) === 6) {
+        currWeather = "눈"
+      } else if (parseInt(currWeatherId / 100) === 7) {
+        currWeather = "흐림"
+      } else if (currWeatherId === 800) {
+        currWeather = "맑음"
+      } else if (parseInt(currWeatherId / 100) === 8) {
+        currWeather = "구름 많음"
+      }
+
+      weatherInfo.innerHTML = `${currTemp}도 / ${currWeather}`;
+
+      //daily
+
+      const daily = data.daily;
+      let dailyList = [];
+
+      for (i = 1; i < daily.length; i++) {
+        const dailyTimestamp = daily[i].dt;
+        const dailyDate = timestampToDate(dailyTimestamp);
+        const dailyTemp = Math.round(daily[i].temp.day - 273.15);
+        const dailyWeather = daily[i].weather[0].id;
+
+        dailyList.push({ date: dailyDate, tem: dailyTemp, weather: dailyWeather });
+      }
+    })
+
+}
+
+const lat = 37.477550020716194;
+const lon = 126.98212524649105;
+
+getWeather(lat, lon);
+
