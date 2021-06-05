a4bce6cff423bc60511efb245e2ef4750eed5f6c
SoHyun <pje1740@gmail.com>
2021-06-05 21:01:41 +0900

    [FEAT] week03 코드 업로드

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const weatherInfo = document.querySelector("#weather-info");
 const weatherImg = document.querySelector("#weather-img");
 const personImg = document.querySelector("#person-img");
 
+function timestampToDate(timestamp) {
+  const fullDate = new Date(timestamp * 1000);
+
+  const month = fullDate.getMonth() + 1;
+  const date = fullDate.getDate();
+
+  return `${month}월 ${date}일`;
+}
+
 // 데이터 가져오기
 // 위도 경도
 const lat = 37.477550020716194;
@@ -18,5 +27,19 @@ fetch(
   })
   .then((data) => {
     // data : 받아온 데이터가 담겨있는 변수
-    console.log(data);
+    // console.log(data);
+
+    const curr = data.current;
+    const currTimestamp = curr.dt;
+    const currDate = timestampToDate(currTimestamp);
+    const currTemp = Math.round(curr.temp - 273.15);
+    const currWeatherId = curr.weather[0].id;
+
+    const today = {
+      date: currDate,
+      tem: currTemp,
+      weather: currWeatherId,
+    };
+
+    title.innerHTML = `${today.date}의 내 기상환경은?`;
   });
